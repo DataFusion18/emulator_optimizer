23cc4b26976147c7ff60ea92619745474afdee2d
Rhys Whitley <rhys.whitley@gmail.com>
2016-06-21 15:23:52 +1000

    tested different algorithms, fit is okay but could be better

--- emulator/build_emulators.py
+++ emulator/build_emulators.py
@@ -1,22 +1,25 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 
 import os, re
 import pickle
 import numpy as np
 import pandas as pd
+import seaborn as sns
 
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
+from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
-from sklearn.pipeline import make_pipeline
+
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
-
 from sklearn.svm import SVR
-from sklearn.grid_search import GridSearchCV
+from sklearn.neighbors import KNeighborsRegressor
 from sklearn import cross_validation
+
+from sklearn.grid_search import GridSearchCV
 
 def get_site_name(fpath):
     return fpath.split("/")[-3]
@@ -47,32 +50,21 @@ def main():
     # BUILD MODELS
 
     # practice set
-    sname = spa_hourly.keys()[0]
-    print sname
+    print(list(spa_hourly.keys()))
+    sname = list(spa_hourly.keys())[0]
+    print(sname)
 
     # the target values it is trying to reproduce
     #target = spa_hourly[sname].ix[:, ["gpp", "lemod"]].values
-    target = np.array(spa_hourly[sname]["gpp"])
+    target = spa_hourly[sname]["gpp"]
     # the drivers of the emulator (crop to match length of target)
     data = natt_datasets[sname]["drivers"] \
-        .ix[:len(target), 1:].values
+        .ix[:len(target), 1:]
 
     # hold back 40% of the dataset for testing
     X_train, X_test, Y_train, Y_test = \
-        cross_validation.train_test_split(data, target, \
-                                          test_size=0.4, random_state=0)
-
-    param_DTR = { \
-        #"min_samples_split": [2, 10, 20], \
-        "max_depth": [2, 5, 10, 30], \
-        #"min_samples_leaf": [1, 5, 10], \
-        #"max_leaf_nodes": [5, 10, 20], \
-        #"criterion": ["gini", "entropy"], \
-        "max_features": [2, 4, 8], \
-        "n_estimators": [5, 10, 20] \
-        }
-    #regmod = DecisionTreeRegressor()
-    regmod = RandomForestRegressor()
+        cross_validation.train_test_split(data.values, target.values, \
+                                          test_size=0.9, random_state=0)
 
 #    param_DTR = { \
 #        #"min_samples_split": [2, 10, 20], \
@@ -80,23 +72,75 @@ def main():
 #        #"min_samples_leaf": [1, 5, 10], \
 #        #"max_leaf_nodes": [5, 10, 20], \
 #        }
-#    regmod = SVR()
+#
+#    param_RFR = { \
+#        #"min_samples_split": [2, 10, 20], \
+#        "max_depth": [None], \
+#        #"min_samples_leaf": [1, 5, 10], \
+#        #"max_leaf_nodes": [5, 10, 20], \
+#        #"criterion": ["gini", "entropy"], \
+#        "max_features": [2, 4, 8], \
+#        #"n_estimators": [5, 10, 20] \
+#        "n_estimators": [300], \
+#
+#    param_SVR = { \
+#        "C": [0.5, 1, 2], \
+#        "kernel": ['rbf'], \
+#        #"coeff": [0, 0.3, 0.5], \
+#        "degree": [1, 2, 3], \
+#        }
+#
+    param_KNR = { \
+        "n_neighbors": [5, 10, 20, 100, 200], \
+        "weights": ['uniform', 'distance'], \
+        }
 
-    emulator = GridSearchCV(regmod, param_grid=param_DTR, cv=5)
+#    regmod = DecisionTreeRegressor()
+#    regmod = RandomForestRegressor()
+#    regmod = SVR()
+    regmod = KNeighborsRegressor()
+
+    pipeit3 = lambda model: make_pipeline(StandardScaler(), PCA(), model)
+    pipeit2 = lambda model: make_pipeline(StandardScaler(), model)
+    regmod_p = pipeit2(regmod)
+
+    modlab = regmod_p.steps[-1][0]
+
+    par_grid = {'{0}__{1}'.format(modlab, parkey): pardat \
+                 for (parkey, pardat) in param_KNR.items()}
+
+    #emulator = GridSearchCV(regmod, param_grid=param_DTR, cv=5)
+    emulator = GridSearchCV(regmod_p, param_grid=par_grid, cv=5)
 
     emulator.fit(X_train, Y_train)
     pred_test = emulator.predict(X_test)
 
-    plt.plot(pred_test, Y_test, 'ro')
+    output_test = pd.DataFrame({'emu': pred_test, \
+                                'spa': Y_test})
+#    sns.jointplot(x='emu', y='spa', data=output_test, kind='reg')
+#    plt.show()
+
+    output_emulated = pd.DataFrame({'DT': data.index, \
+                                    'emu': emulator.predict(data), \
+                                    'spa': target})
+    output_emulated.set_index(['DT'], inplace=True)
+    print(output_emulated.head())
+
+    output_day = output_emulated.resample('D').mean()
+
+    plt.plot_date(output_day.index, output_day['emu'], '-', label='Emulator')
+    plt.plot_date(output_day.index, output_day['spa'], '-', label='SPA')
+    plt.ylabel('GPP ($\mu$mol m$^{-2}$ s$^{-1}$)')
+    plt.legend()
     plt.show()
 
     return None
 
-    gs = gridspec.GridSpec(1, 2)
-    for i in range(2):
-        ax = plt.subplot(gs[i])
-        ax.plot(pred_test[:, i], Y_test[:, i], 'ro')
-    plt.show()
+#    gs = gridspec.GridSpec(1, 2)
+#    for i in range(2):
+#        ax = plt.subplot(gs[i])
+#        ax.plot(pred_test[:, i], Y_test[:, i], 'ro')
+#    plt.show()
 
 # --------------------------------------------------------------------------------
 
